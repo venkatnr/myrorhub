c92bf8c8fd083a5a4f94b1b80b86561704fb3244
lakshmi <lakshmi@lakshmi-Lenovo-G570.(none)>
2012-11-06 07:21:22 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrorhub::Application.config.secret_token = '436a0b231d62faaaf069f604ffd42fc1cc58b1e93f6388d571b907a0458afa33cec47585a22796452037ac919e4a52e8aa81de1718fd65596919a8a0be01c0ba'
