0dcf1b9b89b19ed46524bf10d9aaed66012ad740
venkatnr <nishankararamu@gmail.com>
2012-11-06 13:44:18 +0530

    test one

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Myrorhub::Application.configure do
 
 #config.action_mailer.raise_delivery_errors = false
 config.action_mailer.raise_delivery_errors = false
-	config.action_mailer.default_url_options = { :host => 'http://myrorhub.herokuapp.com/' }
+	config.action_mailer.default_url_options = { :host => 'http://myrorhub.herokuapp.com' }
 	config.action_mailer.perform_deliveries = true
 	config.action_mailer.delivery_method = :smtp
 	config.action_mailer.raise_delivery_errors = true
