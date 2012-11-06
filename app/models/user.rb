4afa3ef4dfcd280b5d9f82ec8aaa4b2e1dcdc061
venkatnr <nishankararamu@gmail.com>
2012-11-06 15:43:57 +0530

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
                          image:auth.info.image
                          )
   end
-  user
+  raise user.insepct
 end
  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
   data = access_token.info
