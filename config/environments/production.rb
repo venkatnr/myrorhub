48e78c5fe6fc1ac8cebe041932f3fa008ee35629
venkatnr <nishankararamu@gmail.com>
2012-11-07 11:08:33 +0530

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ config.action_mailer.smtp_settings = {
 	:address => "smtp.gmail.com",
 	:port=>587,
 	:domain => "gmail.com",
-	:authentication => :login,
+	:authentication => 'plain',
 	:user_name => "redmine.logicmatter",
 	:password => "redminematter",
 }
