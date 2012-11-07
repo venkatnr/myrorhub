04b49c25bc199b5855c44c366fc11752b572b4c7
venkatnr <nishankararamu@gmail.com>
2012-11-07 11:24:14 +0530

    test two

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,14 +57,13 @@ config.action_mailer.raise_delivery_errors = false
 	config.action_mailer.raise_delivery_errors = true
 
 config.action_mailer.smtp_settings = {
-	:enable_starttls_auto => true,
-	:address => "smtp.gmail.com",
-	:port=>587,
-	:domain => "gmail.com",
-	:authentication => 'plain',
-	:user_name => "redmine.logicmatter",
-	:password => "redminematter",
-	:enable_starttls_auto => true,
+						   :address   => "smtp.sendgrid.net",
+                           :port      => 587,
+                           :domain    => "yourdomain.com",
+                           :user_name => "nishankararamu@domain.com",
+                           :password  => "nishankara$4",
+                           :authentication => 'plain',
+                           :enable_starttls_auto => true }
 }
 
   # Enable threaded mode
